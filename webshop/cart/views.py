305ed2f89fb823b893bc503691a91dbd0508782e
Chris <chrsd@gmx.de>
2024-07-14 15:02:58 +0200

    cart merge fixes

--- webshop/cart/views.py
+++ webshop/cart/views.py
@@ -3,66 +3,177 @@ from decimal import Decimal
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from products.models import Product
-from .models import Cart, CartItem
+from .models import ShoppingCart, ShoppingCartItem
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseBadRequest
+
+@login_required
+def show_shopping_cart(request):
+    def create_context_object(
+            shopping_cart_is_empty=True,
+            shopping_cart_items=None,
+            total=0.0
+    ):
+        return {
+            'shopping_cart_is_empty': shopping_cart_is_empty,
+            'shopping_cart_items': shopping_cart_items,
+            'total': Decimal(total)  # The model expects a Decimal, not float!
+        }
+
+    myuser = request.user
+    context = {}
+
+    if myuser.is_authenticated:
+        try:
+            shopping_cart = ShoppingCart.objects.get(myuser=myuser)
+            shopping_cart_items = ShoppingCartItem.objects.filter(shopping_cart=shopping_cart)
+            total = shopping_cart.get_total()
+
+            context = create_context_object(
+                shopping_cart_is_empty=False,
+                shopping_cart_items=shopping_cart_items,
+                total=total
+            )
+        except ObjectDoesNotExist:  # User has no shopping cart
+            # If no shopping cart exists, create an empty context
+            context = create_context_object()
+    else:
+        # If user is not authenticated, create an empty context
+        context = create_context_object()
+
+    return render(request, 'cart/cart.html', context)
 
 
+@login_required
 @login_required
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    cart_id = request.session.get('cart_id')
+    myuser = request.user
 
-    if not cart_id:
-        cart = Cart.objects.create(user=request.user)
-        request.session['cart_id'] = cart.id
-    else:
-        cart = Cart.objects.get(id=cart_id, user=request.user)
+    quantity = int(request.POST.get('quantity', 1))
 
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product, defaults={'quantity': 1, 'item_price': product.price})
+    try:
+        shopping_cart = ShoppingCart.objects.get(myuser=myuser)
+    except ShoppingCart.DoesNotExist:
+        shopping_cart = ShoppingCart.objects.create(myuser=myuser)
 
-    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product, defaults={'quantity': 1, 'item_price': product.price})
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
+    try:
+        shopping_cart_item = ShoppingCartItem.objects.get(
+            shopping_cart=shopping_cart,
+            product_id=product.id
+        )
+        shopping_cart_item.quantity += quantity
+        shopping_cart_item.save()
+    except ShoppingCartItem.DoesNotExist:
+        ShoppingCartItem.objects.create(
+            product_id=product.id,
+            product_name=product.name,
+            price=product.price,
+            quantity=quantity,
+            shopping_cart=shopping_cart
+        )
 
-    cart.update_totals()
-    return redirect('cart:cart')
-
-
-@login_required
-def cart_detail(request):
-    cart = Cart.objects.filter(user=request.user).first()
-    cart_items = cart.items.all() if cart else []
-    total_price = cart.total_price if cart else 0
-    return render(request, 'cart/cart.html', {'cart_items': cart_items, 'total_price': total_price})
-
+    return redirect('cart:shopping-cart-show')
 
 @login_required
 def remove_from_cart(request, item_id):
-    cart_item = get_object_or_404(CartItem, pk=item_id)
-    cart_item.delete()
-    cart_item.cart.update_totals()
-    return redirect('cart:cart')
+    cart_item = get_object_or_404(ShoppingCartItem, pk=item_id)
+    shopping_cart = cart_item.shopping_cart
 
+    if request.method == 'POST':
+        quantity = request.POST.get('quantity')
 
-@login_required
-def checkout(request):
-    cart = Cart.objects.filter(user=request.user).first()
-    cart_items = cart.items.all() if cart else []
-    total_price = cart.total_price if cart else 0
-    return render(request, 'cart/checkout.html', {'cart_items': cart_items, 'total_price': total_price})
+        try:
+            quantity = int(quantity)
+        except ValueError:
+            return HttpResponseBadRequest("Invalid quantity")
 
+        if quantity >= cart_item.quantity:
+            # Remove the item completely if quantity to remove is greater or equal to the item's quantity
+            cart_item.delete()
+        else:
+            # Reduce the quantity of the item
+            cart_item.quantity -= quantity
+            cart_item.save()
 
-@login_required
-def checkout_process(request):
-    cart = Cart.objects.filter(user=request.user).first()
-    if cart:
-        cart.items.all().delete()
-        cart.update_totals()
-    return redirect('cart:cart')
+        # Update the total price in the shopping cart
+        total = shopping_cart.get_total()
+        shopping_cart.total = total
+        shopping_cart.save()
+
+    return redirect('cart:shopping-cart-show')
 
 
 def clear_cart(request):
-    cart_id = request.session.get('cart_id')
-    if cart_id:
-        CartItem.objects.filter(cart_id=cart_id).delete()
-        del request.session['cart_id']
+    try:
+        shopping_cart = ShoppingCart.objects.get(myuser=request.user)
+    except ShoppingCart.DoesNotExist:
+        shopping_cart = None
+    
+    if shopping_cart:
+        shopping_cart.clear_items()
+    
+    return redirect('cart:shopping-cart-show')
+
+
+
+# @login_required
+# def add_to_cart(request, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     cart_id = request.session.get('cart_id')
+
+#     if not cart_id:
+#         cart = Cart.objects.create(user=request.user)
+#         request.session['cart_id'] = cart.id
+#     else:
+#         cart = Cart.objects.get(id=cart_id, user=request.user)
+
+#     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product, defaults={'quantity': 1, 'item_price': product.price})
+
+#     cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product, defaults={'quantity': 1, 'item_price': product.price})
+#     if not created:
+#         cart_item.quantity += 1
+#         cart_item.save()
+
+#     cart.update_totals()
+#     return redirect('cart:cart')
+
+
+# @login_required
+# def cart_detail(request):
+#     cart = Cart.objects.filter(user=request.user).first()
+#     cart_items = cart.items.all() if cart else []
+#     total_price = cart.total_price if cart else 0
+#     return render(request, 'cart/cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+# @login_required
+# def remove_from_cart(request, item_id):
+#     cart_item = get_object_or_404(CartItem, pk=item_id)
+#     cart_item.delete()
+#     cart_item.cart.update_totals()
+#     return redirect('cart:cart')
+
+
+# @login_required
+# def checkout(request):
+#     cart = Cart.objects.filter(user=request.user).first()
+#     cart_items = cart.items.all() if cart else []
+#     total_price = cart.total_price if cart else 0
+#     return render(request, 'cart/checkout.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+# @login_required
+# def checkout_process(request):
+#     cart = Cart.objects.filter(user=request.user).first()
+#     if cart:
+#         cart.items.all().delete()
+#         cart.update_totals()
+#     return redirect('cart:cart')
+
+
+# def clear_cart(request):
+#     cart_id = request.session.get('cart_id')
+#     if cart_id:
+#         CartItem.objects.filter(cart_id=cart_id).delete()
+#         del request.session['cart_id']
